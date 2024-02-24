4a552e0cfdee7a139da45327a4b1ee3eea5904e1
Alxruc <ruchtialex@gmail.com>
2024-02-24 17:11:06 +0100

    added bpm guessing and updated README

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from build.gui import window, playlistButton, randomSongButton, submitButton, bpmEntry, playlistEntry
+from build.gui import *
 from tkinter import StringVar
 
 import spotipy
@@ -9,9 +9,13 @@ import random
 import requests
 import pygame
 import tempfile
+import essentia.standard as es
+import math
 from urllib.parse import urlparse
 
 playlist = None
+current_audio_file = None
+artistAndTitle = " - "
 
 def get_playlist_id(url):
     result = urlparse(url)
@@ -35,19 +39,43 @@ def loadPlaylist(spotify):
     global playlist
     playlistId = get_playlist_id(playlistURL.get())
     if(playlistId != None):
-        print(playlistId)
+        print(f'Grabbed playlist id {playlistId}')
         playlist = spotify.playlist_tracks(playlistId)
+        return
     print("Couldn't load playlist")
     return None 
 
 def getRandomTrack():
     global playlist
+    global current_audio_file
+    global artistAndTitle
+
     if(playlist == None):
         return
+    
+    # Hide label in case it is still visible from last guess
+    check_label.config(fg=backgroundColor)
+    
+    # If there's a currently playing track, delete its file
+    if current_audio_file is not None:
+        os.unlink(current_audio_file)
+        current_audio_file = None
 
     tracks = playlist['items']
     random_track = random.choice(tracks)
     preview_url = random_track['track']['preview_url']
+
+    # Getting the artist(s) and song title to display, truncating it to a max of 35 characters
+    songPlayingString = ""
+    for i in range(0,len(random_track['track']['artists'])):
+        songPlayingString = songPlayingString + random_track['track']['artists'][i]['name'] + ", "
+
+    songPlayingString = songPlayingString[:-2] + " - "  # Remove the last comma and space
+    songPlayingString = songPlayingString + random_track['track']['name']
+
+    if len(songPlayingString) > 35:
+        songPlayingString = songPlayingString[:35] + '...'
+    songPlaying_label.config(text=songPlayingString)
     
     if preview_url is not None:
         # Download the audio file
@@ -56,20 +84,78 @@ def getRandomTrack():
         temp_file.write(response.content)
         temp_file.close()
 
+        current_audio_file = temp_file.name
+
         # Play the audio file
-        pygame.mixer.music.load(temp_file.name)
+        pygame.mixer.music.load(current_audio_file)
         pygame.mixer.music.play()
     else:
-        print("No preview available for this track")
+        songPlaying_label.config(text="No preview available, try again")
+
+def getTempo():
+    global current_audio_file
+    if(current_audio_file is None):
+        return 0
+    
+    audio = es.MonoLoader(filename=current_audio_file)()
+    rhythm_extractor = es.RhythmExtractor2013(method="multifeature")
+    bpm, _, _, _, _ = rhythm_extractor(audio)
+    return bpm
+
+def checkTempo():
+    guess = bpmGuess.get()
+
+    if(len(guess) == 0):
+        print("Please enter a (whole) number")
+        return
+    
+    try:
+        guess = int(guess)
+    except (TypeError, ValueError):
+        print("Please enter a (whole) number")
+        return
+    
+    guess = int(guess)
+    tempo = math.ceil(getTempo())
+    
+    if(tempo == guess):
+        check_label.config(fg='#1DB954', text=f'Correct, the bpm was: {tempo}')
+        return
+    
+    if(guess - 1 <= tempo <= guess + 1):
+        check_label.config(fg='#FFFF00', text=f'Almost, the bpm was: {tempo}')
+        return
+
+    if(guess - 2 <= tempo <= guess + 2):
+        check_label.config(fg='#FFA500', text=f'Close, the bpm was: {tempo}')
+        return
+    
+    check_label.config(fg='#FF0000', text=f'Wrong, the bpm was: {tempo}')
+    return
+
+
+def cleanup():
+    global current_audio_file
+
+    # If there's a currently playing track, delete its file
+    if current_audio_file is not None:
+        os.unlink(current_audio_file)
+        current_audio_file = None
 
 
 if __name__ == '__main__':
-    spotify = initSpotify()
-    pygame.mixer.init()
-    pygame.mixer.music.set_volume(0.05)
-    playlistURL = StringVar()
-    playlistEntry.config(textvariable=playlistURL)
+    try:
+        spotify = initSpotify()
+        pygame.mixer.init()
+        pygame.mixer.music.set_volume(0.05)
+        playlistURL = StringVar()
+        bpmGuess = StringVar()
+        playlistEntry.config(textvariable=playlistURL)
+        bpmEntry.config(textvariable=bpmGuess)
 
-    playlistButton.config(command=lambda: loadPlaylist(spotify))
-    randomSongButton.config(command=lambda: getRandomTrack())
-    window.mainloop()
+        playlistButton.config(command=lambda: loadPlaylist(spotify))
+        randomSongButton.config(command=lambda: getRandomTrack())
+        submitButton.config(command=lambda: checkTempo())
+        window.mainloop()
+    finally:
+        cleanup()
